e224303e09d9007b42e49cb8d08be07dd37e8870
Aishwarya55621 <aaishwaryaapd.125@gmail.com>
2025-05-23 20:21:35 +0530

    Update app.py

--- Ai_support_platform/app.py
+++ Ai_support_platform/app.py
@@ -1,1 +1,21 @@
-Print("hello")
+from flask import Flask, render_template, request, redirect
+from utils import process_text_and_store
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    text = request.form["company_data"]
+    process_text_and_store(text)
+    return "<h3>Data processed and stored successfully. You can now use it in chatbot.</h3>"
+
+if __name__ == "__main__":
+    if not os.path.exists("chroma_db"):
+        os.mkdir("chroma_db")
+    app.run(debug=True)
+
